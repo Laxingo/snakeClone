c615315eed0db4c3a3cef2b43d7835fc801e8432
Laxingo <goncalosilchaves@gmail.com>
2021-04-29 20:40:29 +0100

    ta a funceminar
    lets goooooooooooooooooooooo

--- js/snake.js
+++ js/snake.js
@@ -1,22 +1,26 @@
 export default class Snake {
+   
     constructor(scene){
         this.scene = scene;
+        this.timeLastMove = 0;
+        this.moveInterval = 250;
+
         this.direction = Phaser.Math.Vector2.RIGHT;
         this.body=[];
 
         this.body.push(
             this.scene.add
-                .rectangle(20, 20, 16, 16, 0xff0000)
+                .rectangle(0, 0, 16, 16, 0xff0000)
                 .setOrigin(0)
         );
 
         this.body.push(
             this.scene.add
-                .rectangle(20, 20, 16, 16, 0x0000ff)
+                .rectangle(0, 0, 16, 16, 0x0000ff)
                 .setOrigin(0)
         );
 
-        this.scene.input.keyboard.on('keydown', e=> {
+        this.scene.input.keyboard.on('keydown', e => {
             this.keydown(e);
         });
     }
@@ -32,7 +36,7 @@ export default class Snake {
             case 39:
                 this.direction = Phaser.Math.Vector2.RIGHT;
                 break;
-            case 40:
+             case 40:
                 this.direction = Phaser.Math.Vector2.DOWN;
                 break;
         }
@@ -52,8 +56,15 @@ export default class Snake {
     }
 
     update(time){
-        //console.log('serpenteando...');
-        this.box[0].x += this.direction.x * 16;
-        this.box[0].y += this.direction.y * 16;
+        if(time >= this.timeLastMove + this.moveInterval)
+        {
+            this.timeLastMove = time;
+            this.move();
+        }
+    }
+
+    move(){
+        this.body[0].x += this.direction.x * 16;
+        this.body[0].y += this.direction.y * 16;
     }
 }
